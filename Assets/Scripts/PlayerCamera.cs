0ad6c7f9de7b432700ead8ff5d1180dc6c52915c
Gage Duke <72324996+Duke547@users.noreply.github.com>
2023-06-10 06:39:05 -0400

    Implement movement delay for player camera.

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -6,25 +6,44 @@ namespace VoxelWorld.Scripts
     [RequireComponent(typeof(Camera))]
     public class PlayerCamera : MonoBehaviour
     {
-        Vector3 cameraRotation;
+        Vector3   cameraRotation;
+        Transform originalParent;
+        Vector3   originalLocalPosition;
 
         [Min(0)]
         public float mouseSensitivity = 0.2f;
-        
-        new Camera camera => GetComponent<Camera>();
+
+        private void Start()
+        {
+            originalParent        = transform.parent;
+            originalLocalPosition = transform.localPosition;
+
+            transform.parent = null;
+        }
+
+        void UpdatePosition()
+        {
+            var targetPosition = originalParent.transform.position + originalLocalPosition;
+            
+            transform.position = Vector3.Lerp(transform.position, targetPosition, 10 * Time.deltaTime);
+        }
+
+        void UpdateRotation()
+        {
+            var mouseY       = Mouse.current.delta.y.ReadValue();
+            var currentPitch = cameraRotation.x;
+            var pitchChange  = -mouseY * mouseSensitivity;
+            var newPitch     = Mathf.Clamp(currentPitch + pitchChange, -90, 90);
+            var newYaw       = originalParent.transform.eulerAngles.y;
+
+            cameraRotation        = new(newPitch, newYaw, 0);
+            transform.eulerAngles = cameraRotation;
+        }
 
         void Update()
         {
-            if (camera != null)
-            {
-                var mouseY       = Mouse.current.delta.y.ReadValue();
-                var currentPitch = cameraRotation.x;
-                var amount       = -mouseY * mouseSensitivity;
-                var newPitch     = Mathf.Clamp(currentPitch + amount, -90, 90);
-
-                cameraRotation = new(newPitch, 0, 0);
-                camera.transform.localEulerAngles = cameraRotation;
-            }
+            UpdatePosition();
+            UpdateRotation();
         }
     }
 }
