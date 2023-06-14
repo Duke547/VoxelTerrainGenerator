4c7148c21420c6f3032fd7eb04d5534a9734a0e8
Gage Duke <72324996+Duke547@users.noreply.github.com>
2023-06-14 10:57:59 -0400

    Remove unity profiling from terrain mesh generator.

--- Assets/Classes/TerrainMeshGenerator.cs
+++ Assets/Classes/TerrainMeshGenerator.cs
@@ -1,4 +1,3 @@
-using Unity.Profiling;
 using UnityEngine;
 using static UnityEngine.Vector3;
 
@@ -8,18 +7,15 @@ namespace VoxelWorld.Classes
     {
         static void GenerateBlockFace(MeshCache mesh, Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, Vector3 direction)
         {
-            using (new ProfilerMarker($"{nameof(TerrainMeshGenerator)}.{nameof(GenerateBlockFace)}").Auto())
-            {
-                int i1 = mesh.Vertices.Count;
-                int i2 = i1 + 1;
-                int i3 = i2 + 1;
-                int i4 = i3 + 1;
+            int i1 = mesh.Vertices.Count;
+            int i2 = i1 + 1;
+            int i3 = i2 + 1;
+            int i4 = i3 + 1;
 
-                mesh.Vertices.AddRange(new[] { v1, v2, v3, v4 });
-                mesh.Indices .AddRange(new[] { i1, i2, i3, i3, i4, i1 });
-                mesh.Normals .AddRange(new[] { direction, direction, direction, direction });
-                mesh.UVs     .AddRange(new[] { new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1) });
-            }
+            mesh.Vertices.AddRange(new[] { v1, v2, v3, v4 });
+            mesh.Indices .AddRange(new[] { i1, i2, i3, i3, i4, i1 });
+            mesh.Normals .AddRange(new[] { direction, direction, direction, direction });
+            mesh.UVs     .AddRange(new[] { new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1) });
         }
 
         static void GetBlockFaceVertices(Vector3Int position, Vector3 axis, out Vector3 v1, out Vector3 v2, out Vector3 v3, out Vector3 v4)
@@ -51,69 +47,57 @@ namespace VoxelWorld.Classes
 
         static void TryGenerateAdjacentFace(MeshCache mesh, BlockType first, BlockType second, Vector3Int position, Vector3 axis)
         {
-            using (new ProfilerMarker($"{nameof(TerrainMeshGenerator)}.{nameof(TryGenerateAdjacentFace)}").Auto())
+            if (first.IsSolid != second.IsSolid)
             {
-                if (first.IsSolid != second.IsSolid)
-                {
-                    GetBlockFaceVertices(position, axis, out var v1, out var v2, out var v3, out var v4);
+                GetBlockFaceVertices(position, axis, out var v1, out var v2, out var v3, out var v4);
 
-                    if (first.IsSolid)
-                        GenerateBlockFace(mesh, v1, v2, v3, v4, axis);
-                    else
-                        GenerateBlockFace(mesh, v4, v3, v2, v1, -axis);
-                }
+                if (first.IsSolid)
+                    GenerateBlockFace(mesh, v1, v2, v3, v4, axis);
+                else
+                    GenerateBlockFace(mesh, v4, v3, v2, v1, -axis);
             }
         }
 
         static void GenerateBlock(World world, MeshCache mesh, Vector3Int position)
         {
-            using (new ProfilerMarker($"{nameof(TerrainMeshGenerator)}.{nameof(GenerateBlock)}").Auto())
-            {
-                var current = world.GetBlock(Vector3Int.RoundToInt(position));
+            var current = world.GetBlock(Vector3Int.RoundToInt(position));
 
-                if (current == null)
-                    return;
+            if (current == null)
+                return;
 
-                var rightAdjacent = world.GetBlock(Vector3Int.RoundToInt(position + right  ));
-                var topAdjacent   = world.GetBlock(Vector3Int.RoundToInt(position + up     ));
-                var frontAdjacent = world.GetBlock(Vector3Int.RoundToInt(position + forward));
+            var rightAdjacent = world.GetBlock(Vector3Int.RoundToInt(position + right  ));
+            var topAdjacent   = world.GetBlock(Vector3Int.RoundToInt(position + up     ));
+            var frontAdjacent = world.GetBlock(Vector3Int.RoundToInt(position + forward));
 
-                if (rightAdjacent != null)
-                    TryGenerateAdjacentFace(mesh, current, rightAdjacent, position, right);
-                
-                if (topAdjacent != null)
-                    TryGenerateAdjacentFace(mesh, current, topAdjacent,   position, up);
+            if (rightAdjacent != null)
+                TryGenerateAdjacentFace(mesh, current, rightAdjacent, position, right);
+            
+            if (topAdjacent != null)
+                TryGenerateAdjacentFace(mesh, current, topAdjacent,   position, up);
 
-                if (frontAdjacent != null)
-                    TryGenerateAdjacentFace(mesh, current, frontAdjacent, position, forward);
-            }
+            if (frontAdjacent != null)
+                TryGenerateAdjacentFace(mesh, current, frontAdjacent, position, forward);
         }
 
         static void GenerateChunkBlocks(World world, MeshCache mesh, RectInt rect)
         {
-            using (new ProfilerMarker($"{nameof(TerrainMeshGenerator)}.GenerateChunkBlocks").Auto())
+            for (int z = rect.y; z < rect.height + rect.y; z++)
             {
-                for (int z = rect.y; z < rect.height + rect.y; z++)
+                for (int y = 0; y < world.Height; y++)
                 {
-                    for (int y = 0; y < world.Height; y++)
-                    {
-                        for (int x = rect.x; x < rect.width + rect.x; x++)
-                            GenerateBlock(world, mesh, new(x, y, z));
-                    }
+                    for (int x = rect.x; x < rect.width + rect.x; x++)
+                        GenerateBlock(world, mesh, new(x, y, z));
                 }
             }
         }
 
         public static MeshCache GenerateChunkMesh(World world, RectInt rect)
         {
-            using (new ProfilerMarker($"{nameof(TerrainMeshGenerator)}.GenerateChunkMesh").Auto())
-            {
-                var mesh = new MeshCache();
+            var mesh = new MeshCache();
 
-                GenerateChunkBlocks(world, mesh, rect);
+            GenerateChunkBlocks(world, mesh, rect);
 
-                return mesh;
-            }
+            return mesh;
         }
     }
 }
