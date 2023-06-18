41323653d8ccc37f21e714048eb28fe41dec106c
Gage Duke <72324996+Duke547@users.noreply.github.com>
2023-06-18 00:29:32 -0400

    Revert "Store mesh data in dictionaries."
    This reverts commit 3e9a2c26390aa2b9a5c1d7fe29a34a982a5d42ba.

--- Assets/Classes/MeshCache.cs
+++ Assets/Classes/MeshCache.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,15 +6,15 @@ namespace VoxelWorld.Classes
 {
     public class MeshCache
     {
-        public Dictionary<int, Vector3> Vertices { get; } = new();
+        public List<Vector3> Vertices { get; } = new();
 
         public List<int> Indices { get; } = new();
 
-        public Dictionary<int, Vector3> Normals { get; } = new();
+        public List<Vector3> Normals { get; } = new();
 
-        public Dictionary<int, Vector2> UVs { get; } = new();
+        public List<Vector2> UVs { get; } = new();
 
-        public Dictionary<int, Color> Colors { get; } = new();
+        public List<Color> Colors { get; } = new();
 
         public MeshTopology topology { get; set; } = MeshTopology.Triangles;
 
@@ -23,12 +22,11 @@ namespace VoxelWorld.Classes
         {
             var mesh = new Mesh() { indexFormat = IndexFormat.UInt32 };
 
-            mesh.SetVertices(Vertices.Values.ToArray());
-            mesh.SetNormals (Normals .Values.ToArray());
-            mesh.SetUVs     (0, UVs  .Values.ToArray());
-            mesh.SetColors  (Colors  .Values.ToArray());
-            
-            mesh.SetIndices (Indices, topology, 0);
+            mesh.SetVertices(Vertices);
+            mesh.SetIndices(Indices, topology, 0);
+            mesh.SetNormals(Normals);
+            mesh.SetUVs(0, UVs);
+            mesh.SetColors(Colors);
 
             return mesh;
         }
