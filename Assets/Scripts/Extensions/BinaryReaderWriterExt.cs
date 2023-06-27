535f5ce3cec7322955a1abf54b40d18bd262f572
Gage Duke <72324996+Duke547@users.noreply.github.com>
2023-06-27 12:51:15 -0400

    Implement binary serialization for Vector2.

--- Assets/Scripts/Extensions/BinaryReaderWriterExt.cs
+++ Assets/Scripts/Extensions/BinaryReaderWriterExt.cs
@@ -5,6 +5,20 @@ namespace VoxelWorld
 {
     public static class BinaryReaderWriterExt
     {
+        public static void WriteVector2(this BinaryWriter writer, Vector2 vector)
+        {
+            writer.Write(vector.x);
+            writer.Write(vector.y);
+        }
+
+        public static Vector2 ReadVector2(this BinaryReader reader)
+        {
+            var x = reader.ReadSingle();
+            var y = reader.ReadSingle();
+
+            return new(x, y);
+        }
+
         public static void WriteVector3(this BinaryWriter writer, Vector3 vector)
         {
             writer.Write(vector.x);
--- Assets/Tests/Extensions/BinaryReaderWriterExtTests.cs
+++ Assets/Tests/Extensions/BinaryReaderWriterExtTests.cs
@@ -8,6 +8,33 @@ namespace VoxelWorld.Testing
     public class BinaryReaderWriterExtTests
     {
         [Test]
+        public void Vector2_Test()
+        {
+            var file   = ".//Assets/Tests/TestData/Vector2";
+            var input  = new Vector2(1, 2);
+            var output = new Vector2();
+
+            using var writeStream = File.Open(file, FileMode.Create);
+            {
+                using var writer = new BinaryWriter(writeStream, Encoding.UTF8, false);
+                {
+                    writer.WriteVector2(input);
+                }
+            }
+
+            using var readStream = File.Open(file, FileMode.Open);
+            {
+                using var reader = new BinaryReader(readStream, Encoding.UTF8, false);
+                {
+                    output = reader.ReadVector2();
+                }
+            }
+
+            Assert.That(output.x, Is.EqualTo(input.x), "X");
+            Assert.That(output.y, Is.EqualTo(input.y), "Y");
+        }
+
+        [Test]
         public void Vector3_Test()
         {
             var file   = ".//Assets/Tests/TestData/Vector3";
